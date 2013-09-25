75c7ef87f2a5734ec629578f52adefd38b2d6aa8
Becktor <JonathanBecktor@gmail.com>
2013-09-25 14:48:09 +0200

    ænding

--- ECG/ECG/searchPeaks.c
+++ ECG/ECG/searchPeaks.c
@@ -41,18 +41,51 @@ int searchPeaks(){
     IntervalCounter++;
     
    
-    THRESHOLD1 =  NPKF + 0.25 * (SPKF - NPKF);
-    
-    
     if (THRESHOLD1 < peaksX) {
         
+        CalcRR
         
-        
+        if (RR_low<RR<RR_high){
+            Store peak as Rpeak
+            SPKF=0.125+0.875*NPKF
+            Store RR in recentRR_OK
+            Store RR in recentRR
+            RR_AVARAGE2 = avg of RecentRR_OK
+            RR_AVARAGE1 = avg of RecentRR
+            RR_LOW = 0.9*RR_AVARAGE2
+            RR_HIGH = 1.16*RR_AVARAGE2
+            RR_MISS = 1.66*RR_AVARAGE2
+            THRESHOLD1 = NPKF + 0.25*(SPKF-NPKF)
+            THRESHOLD2 = 0.5*THRESHOLD1
+        }
+        else{
+        while (1) {
+   
+            if (RR>RR_MISS) {
+                SearchBackwards through PEAKS and return peak2
+                if (peaks2>THRESHOLD2) {
+                    break
+                }
+            }
+        }
+        Store peak as Rpeak
+        SPKF = 0.25*peak+0.75*SPKF
+        STORE RR in RecentRR
+        RR_AVARAGE1 = avg of RecentRR
+        RR_LOW = 0.9*RR_AVARAGE2
+        RR_HIGH = 1.16*RR_AVARAGE2
+        RR_MISS = 1.66*RR_AVARAGE2
+        THRESHOLD1 = NPKF + 0.25*(SPKF-NPKF)
+        THRESHOLD2 = 0.5*THRESHOLD1
         
         
     }
-    SPKF = 0.125 * peaksX + 0.875 * SPKF;
+    
     NPKF = 0.125 * peaksX + 0.875 * NPKF;
+    THRESHOLD1 = NPKF + 0.25*(SPKF-NPKF);
+    THRESHOLD2 = 0.5*THRESHOLD1;
+    }
+    
     
 //    Peaks counter
     if ( peaksXn[2] < peaksXn[1] && peaksXn[1] > peaksXn[0]){
