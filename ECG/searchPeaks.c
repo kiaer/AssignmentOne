d25b5b1cf4b91618889f4522689cad1f7270aaeb
cebonet <cebrail@Cebrails-MacBook-Air.local>
2013-09-25 14:15:21 +0200

    searchPeaks updated

--- ECG/searchPeaks.c
+++ ECG/searchPeaks.c
@@ -11,29 +11,67 @@
 #include "motionWI.h"
 
     int peaksXn[3];
+    int RR_PEAKS[8];
+    int RR_PEAKS_HIGH[8];
+    int RR_PEAKS[8];
     int peaksX;
-    int countPeaks;
+    int SPKF;
+    int NPKF;
+    int THRESHOLD1;
+    int THRESHOLD2;
+    int RR_AVARAGE1;
+    int RR_AVARAGE2;
+    int RR_LOW;
+    int RR_HIGH;
+    int RR_MISS;
+    int RR_INTERVAL;
+    int Interval;
+    int IntervalCounter;
 
-// define
-// [0] = Xn+1
-// [1] = Xn
-// [2] = Xn-1
+
+/* define
+ [0] = Xn+1
+ [1] = Xn
+ [2] = Xn-1 
+ */
 
 int searchPeaks(){
     
     peaksX = motionWI();
+    IntervalCounter++;
     
-    if ( peaksXn[2] < peaksXn[1] && peaksXn[1] > peaksXn[0]){
-        countPeaks++;
-        printf("true");
+   
+    THRESHOLD1 =  NPKF + 0.25 * (SPKF - NPKF);
+    
+    
+    if (THRESHOLD1 < peaksX) {
+        
+        
+        
+        
         
     }
+    SPKF = 0.125 * peaksX + 0.875 * SPKF;
+    NPKF = 0.125 * peaksX + 0.875 * NPKF;
     
+//    Peaks counter
+    if ( peaksXn[2] < peaksXn[1] && peaksXn[1] > peaksXn[0]){
+    //calculate the threshold
+        
+        
+        
+       
+    }
+
+
+
     peaksXn[2] = peaksXn[1];
     peaksXn[1]= peaksXn[0];
     peaksXn[0]= peaksX;
+
     
-    
-    
-    return countPeaks;
+    return RR_INTERVAL;
 }
+
+
+
